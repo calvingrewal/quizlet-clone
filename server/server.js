caff6f50ddad71f5641907fd7402ec03b86ad209
AlligatorTaco <awesomeduude@gmail.com>
2016-12-17 00:20:04 -0800

    prepared for heroku deploy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname,'..','/public/index.html'))
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('******************LISTENING ON PORT 3000******************');
 })
