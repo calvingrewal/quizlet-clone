b75c09c3fd8e695374f686dcc0af50977545134f
AlligatorTaco <awesomeduude@gmail.com>
2016-12-17 10:26:46 -0800

    set up flashcards api

---
+++ server/flashcards.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'US_HISTORY': [
+    {
+      'term': 'Georgia',
+      'definition': 'Established as a buffer state'
+    },
+    {
+      'term': 'Iraquois Confederation',
+      'definition': 'Largest Native American confederation'
+    },
+    {
+      'term': 'Puritans',
+      'definition': 'Wanted to purify the Church of England'
+    },
+    {
+      'term': 'Fundamental Orders',
+      'definition': 'First modern constitution established in Connecticut'
+    }
+  ],
+  'MATH': [
+    {
+      'term': '1 + 1 = ?',
+      'definition': '2'
+    },
+    {
+      'term': '8 * 3 = ?',
+      'definition': '24'
+    },
+    {
+      'term': '28 / 7 = ?',
+      'definition': '4'
+    },
+    {
+      'term': '13 - 8 = ?',
+      'definition': '5'
+    },
+  ]
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 var express = require('express')
 var path = require('path')
 
+var flashcards = require('./flashcards')
+
 var app = express()
 
 app.use('/public',express.static(path.join(__dirname,'..','/public')))
 
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname,'..','/public/index.html'))
+})
+
+app.get('/flashcards', function(req,res) {
+
+  switch(req.query.type) {
+    case 'US_HISTORY':
+      return res.json({
+        'flashcards': flashcards['US_HISTORY']
+      })
+    case 'MATH':
+      return res.json({
+        'flashcards': flashcards['MATH']
+      })
+    default:
+      return res.json({
+        'flashcards': flashcards['MATH']
+      })
+  }
 })
 
 app.listen(process.env.PORT || 3000, function() {
