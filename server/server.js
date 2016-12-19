2e2aa24af19ab3ea812b94d881da63437cb528ec
AlligatorTaco <awesomeduude@gmail.com>
2016-12-18 22:02:32 -0800

    flashcard data is stored in separate file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var path = require('path')
 
-var flashcards = require('./flashcards')
-
 var app = express()
 
 app.use('/public',express.static(path.join(__dirname,'..','/public')))
@@ -11,23 +9,6 @@ app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname,'..','/public/index.html'))
 })
 
-app.get('/flashcards', function(req,res) {
-
-  switch(req.query.type) {
-    case 'US_HISTORY':
-      return res.json({
-        'flashcards': flashcards['US_HISTORY']
-      })
-    case 'MATH':
-      return res.json({
-        'flashcards': flashcards['MATH']
-      })
-    default:
-      return res.json({
-        'flashcards': flashcards['MATH']
-      })
-  }
-})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('******************LISTENING ON PORT 3000******************');
