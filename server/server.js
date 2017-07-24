8b0f4470223247c923cd0f060dd0baec6fc5d635
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-24 07:54:31 +0000

    ready for deploy on calvingrewal.com

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ var path = require('path')
 
 var app = express()
 
-app.use('/public',express.static(path.join(__dirname,'..','/public')))
+app.use('/public',express.static(path.join(__dirname,'/public')))
 
-app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname,'..','/public/index.html'))
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname,'index.html'))
 })
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('******************LISTENING ON PORT 3000******************');
+app.listen(process.env.PORT || 3001, function() {
+  console.log('******************LISTENING ON PORT 3001******************');
 })
